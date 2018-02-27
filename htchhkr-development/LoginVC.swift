e75062b087f2332f8af4c39743a8f1df859bdd04
Mustafa GUNES <gunes149@gmail.com>
2018-02-27 15:54:04 +0300

    Simple logic hiding status bar

--- htchhkr-development/LoginVC.swift
+++ htchhkr-development/LoginVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    private var hideStatusBar: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,11 +19,25 @@ class LoginVC: UIViewController {
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap(sender:)))
         self.view.addGestureRecognizer(tap)
+
+        hideStatusBar = true
+        
+        UIView.animate(withDuration: 0.3) {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
     }
     
     func handleScreenTap(sender : UITapGestureRecognizer) {
         
         self.view.endEditing(true)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return hideStatusBar
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
     }
     
     @IBAction func cancelBtnClicked(_ sender: Any) {
