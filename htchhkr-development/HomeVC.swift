8e929bf475207feeddd54bbc628f6c4abe17845a
Mustafa GUNES <gunes149@gmail.com>
2018-03-09 10:22:41 +0300

    Zooming in on MKMapView

--- htchhkr-development/HomeVC.swift
+++ htchhkr-development/HomeVC.swift
@@ -145,8 +145,28 @@ class HomeVC: UIViewController {
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
         
-        centerMapOnUserLocation()
-        centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+        DataService.instance.REF_USERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let userSnapshot = snapshot.children.allObjects as? [DataSnapshot]
+            {
+                for user in userSnapshot
+                {
+                    if user.key == self.currentUserId!
+                    {
+                        if user.hasChild("tripCoordinate")
+                        {
+                            self.zoom(toFitAnnotationsFromMapView: self.mapView)
+                            self.centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+                        }
+                        else
+                        {
+                            self.centerMapOnUserLocation()
+                            self.centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+                        }
+                    }
+                }
+            }
+        })
     }
     
     @IBAction func MenuBtnWasClicked(_ sender: Any) {
@@ -228,6 +248,8 @@ extension HomeVC : MKMapViewDelegate {
         lineRenderer.strokeColor = UIColor(displayP3Red: 216/255, green: 71/255, blue: 30/255, alpha: 0.75)
         lineRenderer.lineWidth = 3
         
+        zoom(toFitAnnotationsFromMapView: self.mapView)
+        
         return lineRenderer
     }
     
@@ -304,6 +326,30 @@ extension HomeVC : MKMapViewDelegate {
             
             self.shouldPresentLoadingView(false) // Loading view (Activity Indicator)
         }
+    }
+    
+    func zoom(toFitAnnotationsFromMapView mapView : MKMapView) {
+        
+        if mapView.annotations.count == 0
+        {
+            return
+        }
+        
+        var topLeftCoordinate = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+        var bottomRightCoordinate = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+        
+        for annotation in mapView.annotations where !annotation.isKind(of: DriverAnnotation.self)
+        {
+            topLeftCoordinate.longitude = fmin(topLeftCoordinate.longitude, annotation.coordinate.longitude)
+            topLeftCoordinate.latitude = fmax(topLeftCoordinate.latitude, annotation.coordinate.latitude)
+            bottomRightCoordinate.longitude = fmax(bottomRightCoordinate.longitude, annotation.coordinate.longitude)
+            bottomRightCoordinate.latitude = fmin(bottomRightCoordinate.latitude, annotation.coordinate.latitude)
+        }
+        
+        var region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(topLeftCoordinate.latitude - (topLeftCoordinate.latitude - bottomRightCoordinate.latitude) * 0.5, topLeftCoordinate.longitude + (bottomRightCoordinate.longitude - topLeftCoordinate.longitude) * 0.5), span: MKCoordinateSpan(latitudeDelta: fabs(topLeftCoordinate.latitude - bottomRightCoordinate.latitude) * 2.0, longitudeDelta: fabs(bottomRightCoordinate.longitude - topLeftCoordinate.longitude) * 2.0))
+        
+        region = mapView.regionThatFits(region)
+        mapView.setRegion(region, animated: true)
     }
 }
 
